3c31341bdc23a2bca3e8ad94b0d60a514f132fa4
kourosh s <kourosh23@hotmail.com>
2023-08-26 09:25:51 +0200

    send to self

---
+++ Commands/Command.cs
@@ -0,0 +1,7 @@
+﻿namespace Commands
+{
+    public class Command : ICommand
+    {
+        public string Id { get; set; } = string.Empty;
+    }
+}
---
+++ SendToSelf/Program.cs
@@ -0,0 +1,60 @@
+﻿using Commands;
+using NServiceBus.Logging;
+
+Console.Title = "SendToSelf";
+
+//variables
+IEndpointInstance _endpointInstance = default!;
+ILog _log = LogManager.GetLogger<Program>();
+var _hasFinished = false;
+
+await start();
+
+while (_hasFinished == false)
+{
+    _log.Info("\n>>> Press 'S' to Send Message to self, or 'Q' to quit.");
+    var key = Console.ReadKey();
+    Console.WriteLine();
+
+    switch (key.Key)
+    {
+        case ConsoleKey.S:
+            // Instantiate the command
+            var command = new Command
+            {
+                Id = Guid.NewGuid().ToString()
+            };
+
+            // Send the command to the local endpoint
+            _log.Info($"Sending command, Id = {command.Id}");
+            await _endpointInstance.SendLocal(command).ConfigureAwait(false);
+
+            break;
+
+        case ConsoleKey.Q:
+            _hasFinished = true;
+            break;
+
+        default:
+            _log.Info("Unknown input. Please try again.");
+            break;
+    }
+}
+
+await stop();
+
+async Task start()
+{
+    var endpointConfiguration = new EndpointConfiguration("Endpoint Name");
+    //var endpointConfiguration = new EndpointConfiguration("SendToSelf");
+    // Choose JSON to serialize and deserialize messages
+    //endpointConfiguration.UseSerialization<SystemJsonSerializer>();
+    endpointConfiguration.UseTransport<LearningTransport>();
+    //var transport = endpointConfiguration.UseTransport<LearningTransport>();
+    _endpointInstance = await Endpoint.Start(endpointConfiguration).ConfigureAwait(false);
+}
+
+async Task stop()
+{
+    await _endpointInstance.Stop().ConfigureAwait(false);
+}
